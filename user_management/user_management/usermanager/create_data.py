64277be11df06ab26273f708b2444c7ae6bd5a0f
harish_upadhyay <inimitableharish@gmail.com>
2017-01-15 21:59:36 +0530

    update user, role and permission information

--- user_management/user_management/usermanager/create_data.py
+++ user_management/user_management/usermanager/create_data.py
@@ -2,6 +2,14 @@ import mmap
 import json 
 
 with open('userapp.txt','wb') as userapp_file:
+    #user
     userapp_file.write(json.dumps(str({"id":"user1","roles":["role1","role3"]})))
-
+    #roles
+    userapp_file.write(json.dumps(str({"id":"role1","permissions":["perm1","perm5"]})))
+    userapp_file.write(json.dumps(str({"id":"role3","permissions":["perm6","perm7"]})))
+    #permissions
+    userapp_file.write(json.dumps(str({"id":"perm1","name":"Can check balance"})))
+    userapp_file.write(json.dumps(str({"id":"perm5","name":"Can deposit"})))
+    userapp_file.write(json.dumps(str({"id":"perm6","name":"Can Transfer"})))
+    userapp_file.write(json.dumps(str({"id":"perm7","name":"Can withdraw"})))
 
