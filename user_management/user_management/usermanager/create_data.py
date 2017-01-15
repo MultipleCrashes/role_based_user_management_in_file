f72ed4db279eea4e39249921f2bd50ab95cf74ab
harish_upadhyay <inimitableharish@gmail.com>
2017-01-15 22:12:05 +0530

    modified to be in diff lines

--- user_management/user_management/usermanager/create_data.py
+++ user_management/user_management/usermanager/create_data.py
@@ -3,13 +3,19 @@ import json
 
 with open('userapp.txt','wb') as userapp_file:
     #user
-    userapp_file.write(json.dumps(str({"id":"user1","roles":["role1","role3"]})))
+    userapp_file.write(json.dumps({"id":"user1","roles":["role1","role3"]}))
+    userapp_file.write("\n")
     #roles
     userapp_file.write(json.dumps(str({"id":"role1","permissions":["perm1","perm5"]})))
+    userapp_file.write("\n")
     userapp_file.write(json.dumps(str({"id":"role3","permissions":["perm6","perm7"]})))
+    userapp_file.write("\n")
     #permissions
     userapp_file.write(json.dumps(str({"id":"perm1","name":"Can check balance"})))
+    userapp_file.write("\n")
     userapp_file.write(json.dumps(str({"id":"perm5","name":"Can deposit"})))
+    userapp_file.write("\n")
     userapp_file.write(json.dumps(str({"id":"perm6","name":"Can Transfer"})))
+    userapp_file.write("\n")
     userapp_file.write(json.dumps(str({"id":"perm7","name":"Can withdraw"})))
 
