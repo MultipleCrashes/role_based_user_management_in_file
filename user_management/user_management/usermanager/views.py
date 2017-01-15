2ac59c50a066d04709116f3c091a05b093bb675f
harish_upadhyay <inimitableharish@gmail.com>
2017-01-16 00:46:54 +0530

    added file reading

--- user_management/user_management/usermanager/views.py
+++ user_management/user_management/usermanager/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse 
 # Create your views here.
+import os 
+pwd = '/home/biswajit/user_management_project/user_management/user_management/usermanager/'
+print "pwd ",pwd
 import json 
+
+user_file_name = 'userapp.txt'
 
 def get_user_permissions_list(request):
     response = {"res_str":"","res_code":0}
@@ -12,4 +17,8 @@ def get_user_permissions_list(request):
             response['res_str'] ="Mandatory param missing"
     except Exception as e:
             response['res_str']=str(e)
+    with open(pwd+user_file_name,'r+b') as userfile:
+        for lines in userfile:
+            lines = json.loads(lines)
+            print lines
     return HttpResponse(json.dumps(response))
