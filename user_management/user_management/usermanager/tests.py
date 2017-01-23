0603be8786a8453ee400396ec3b581cdea187f52
harish_upadhyay <inimitableharish@gmail.com>
2017-01-23 15:35:16 +0530

    tests

--- user_management/user_management/usermanager/tests.py
+++ user_management/user_management/usermanager/tests.py
@@ -19,17 +19,20 @@ class UserManagerTestCase(TestCase):
             user_permissions_name = requests.get(API_SERVER+'usermanager/user/?user='+user)
         except Exception as e:
             print "Exception found while hitting get api->",str(e)
-        print "Response for get request  ->",user_permissions_name.text
-        print "Expecte response text     -> {\"res_str\": [\"Can check balance\", \"Can deposit\", \"Can Transfer\", \"Can withdraw\"], \"res_code\": 200}"
         print "Status code of get request-> ",user_permissions_name.status_code
         print "Expected status code :    ->  200 "
         self.assertEqual(user_permissions_name.status_code,200)
+        print "="*20
 
     def test_user_api_response_str(self,user='user1'):
+        print "Sending get request -> ",API_SERVER+'usermanager/user/?user='+user
         try:
             user_permissions_name = requests.get(API_SERVER+'usermanager/user/?user='+user)
         except Exception as e:
             print "Exception found while hitting get api->",str(e)
         res = json.loads(user_permissions_name.text)
         #response string validation
+        print "Response for get request  ->",user_permissions_name.text
+        print "Expected response text     -> {\"res_str\": [\"Can check balance\", \"Can deposit\", \"Can Transfer\", \"Can withdraw\"], \"res_code\": 200}"
         self.assertEquals(str(res['res_str']),str([u'Can check balance', u'Can deposit', u'Can Transfer', u'Can withdraw']))
+        print "="*20
